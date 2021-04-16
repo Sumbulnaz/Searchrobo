75066105d939ff708b469b85721df8469a41ce50
Sumbul Naz <sumbul.1304@gmail.com>
2021-04-16 13:11:52 +0530

    Updates

---
+++ precache-manifest.1f95e8b43f4a94ecec03ab61d537590b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9819886759a79fd7bfcbcb5bfeaa98c",
+    "url": "/Searchrobo/index.html"
+  },
+  {
+    "revision": "51276ed9e717ef08ec48",
+    "url": "/Searchrobo/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "52a5568e943a5228f9e9",
+    "url": "/Searchrobo/static/css/main.bd5fa65a.chunk.css"
+  },
+  {
+    "revision": "51276ed9e717ef08ec48",
+    "url": "/Searchrobo/static/js/2.a687f2ae.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/Searchrobo/static/js/2.a687f2ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52a5568e943a5228f9e9",
+    "url": "/Searchrobo/static/js/main.60bfb1e8.chunk.js"
+  },
+  {
+    "revision": "83984e94b3659b2e4f84",
+    "url": "/Searchrobo/static/js/runtime-main.a6a6ca61.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Searchrobo/static/media/SEGA.12a3db8b.woff"
+  }
+]);
